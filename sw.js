289cfcfded23b5d7e3416a6e099e3f5f623712ac
josefmvalek <valekjo.2026@guh.cz>
2026-04-10 15:21:45 +0200

    service worker setting

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kiscord-v23';
+const CACHE_NAME = 'kiscord-v24';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
@@ -20,7 +20,7 @@ self.addEventListener('install', (event) => {
     self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            console.log('[SW] Caching system assets (v8)');
+            console.log('[SW] Caching system assets (v24)');
             return cache.addAll(ASSETS_TO_CACHE);
         })
     );
